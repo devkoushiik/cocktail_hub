c86db3e08cd7496ad769b648c2d93f4ef1308bc1
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-23 16:53:50 +0600

    basic page setup done

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./About";
+export { default as Cocktail } from "./Cocktail";
+export { default as Error } from "./Error";
+export { default as HomeLayout } from "./HomeLayout";
+export { default as Loading } from "./Loading";
+export { default as NewsLatter } from "./NewsLatter";
