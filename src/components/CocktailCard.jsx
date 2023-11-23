fb167f8600268fd453edd076922d35152a2e1315
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-23 20:32:12 +0600

    cocktail card implemented.

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,4 +1,19 @@
-const CocktailCard = () => {
-  return <div>CocktailCard</div>;
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
+
+const CocktailCard = ({ image, name, id, info, glass }) => {
+  return (
+    <Wrapper>
+      <img src={image} alt={name} className="img"></img>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link className="btn" to={`/cocktail/${id}`}>
+          Details
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default CocktailCard;
