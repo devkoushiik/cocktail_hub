c349356363dfd0cd4b28743a02f442cee1a2f827
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-25 15:09:43 +0600

    loading spinner added.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Navbar } from "../components";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+
   return (
     <>
       <Navbar />
       <section className="page">
+        {isPageLoading ? <div className="loading" /> : <Outlet />}
         <Outlet />
       </section>
     </>
